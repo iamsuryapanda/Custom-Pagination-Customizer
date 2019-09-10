e3627f83cf674d48f6191efe86f114814542be00
suryapanda4 <55087897+suryapanda4@users.noreply.github.com>
2019-09-10 18:55:45 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+function pagination_bar( $custom_query ) {
+
+    $total_pages = $custom_query->max_num_pages;
+    $big = 999999999; // need an unlikely integer
+
+    if ($total_pages > 1){
+        $current_page = max(1, get_query_var('page'));
+
+        echo paginate_links(array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?page=%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+        ));
+    }
+}
