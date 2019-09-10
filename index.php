5dff2d504e3d44e95b2c72d202c50e256eefeaee
suryapanda4 <55087897+suryapanda4@users.noreply.github.com>
2019-09-10 18:59:27 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+  global $paged;
+  if ( get_query_var( 'paged' ) ) { 
+      $paged = get_query_var( 'paged' ); 
+  } elseif ( get_query_var( 'page' ) ) { 
+      $paged = get_query_var( 'page' ); 
+  } else { 
+      $paged = 1; 
+  }                        
+  $test = new WP_Query(array(
+                  'post_type' => 'post',
+                  'paged'=>$paged
+              ));
+  if ( $test->have_posts() ) {
+    while($test->have_posts()):$test->the_post();
+
+    //Loop Content
+
+    endwhile;    
+    pagination_bar( $blogpost ); //Pagination
+    wp_reset_postdata(); 
+  }
+?>
